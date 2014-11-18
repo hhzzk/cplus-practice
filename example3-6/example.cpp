29bd3468dddba530f3dcfa2da5ae4e59a368479f
Jinrui Wang <wangjinrui2013@gmail.com>
2014-11-18 19:55:40 +0800

    Add example3-6

---
+++ example3-6/example.cpp
@@ -0,0 +1,65 @@
+/*************************************************************************
+    > File Name: example.cpp
+    > Author: king
+    > Mail: wangjinrui2013@gmail.com 
+    > Created Time: 2014年11月18日 星期二 19时37分10秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Namelist
+{
+    public:
+        Namelist() { size = 0; p = 0; }
+        Namelist( const string[], int );
+        void set( const string&, int );
+        void set( char*, int );
+        void dump() const;
+    private:
+        int size;
+        string* p;
+};
+
+Namelist::Namelist( const string s[], int si )
+{
+    p = new string[ size = si ];
+    for ( int i = 0; i < size; i++ )
+    {
+        p[ i ] = s[ i ];
+    }
+}
+
+void Namelist::set( const string& s, int i)
+{
+    p[ i ] = s;
+}
+
+void Namelist::set( char* s, int i)
+{
+    p[ i ] = s;
+}
+
+void Namelist::dump() const
+{
+    for( int i = 0; i < size; i++ )
+    {
+        cout << p[ i ] ;
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    string list[] = { "Lab", "Husky", "Collie" };
+    Namelist d1( list, 3 );
+    d1.dump();
+    Namelist d2( d1 );
+    d2.dump();
+    d2.set( "Great Dane", 1 );
+    d2.dump();
+    d1.dump();
+    return 0;
+}
